d54c18faa5b5094d6dc6bfa5313680834c5b4130
Emily Ma <em.kayama@gmail.com>
2020-04-25 16:17:14 -0400

    add procfile, update ports

--- app.js
+++ app.js
@@ -121,6 +121,11 @@ app.post("/delete", (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("server is running on port 3000");
+let port = process.env.PORT;
+if (port == null || prot == "") {
+    port = 3000;
+}
+
+app.listen(port, () => {
+    console.log("server is running successfully");
 });
